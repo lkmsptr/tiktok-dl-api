1eea54f136b245ee467a562cd25db3074835ed50
lkmsptr <lumbershit@gmail.com>
2024-06-14 17:24:12 +0700

    Api fixed

--- src/getData.js
+++ src/getData.js
@@ -1,48 +1,40 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
-const FormData = require("form-data");
-let data = new FormData();
 
 const getData = async (url) => {
   try {
-    data.append("prefix", "dtGslxrcdcG9raW8uY29t");
-    data.append("vid", url);
+    const axios = require("axios");
+    let data = JSON.stringify({
+      url: url,
+    });
+
     let config = {
       method: "post",
       maxBodyLength: Infinity,
-      url: "https://tiktokio.com/api/v1/tk-htmx",
+      url: "https://downloader.bot/api/tiktok/info",
       headers: {
-        ...data.getHeaders(),
+        "Content-Type": "application/json",
+        Cookie: "uid=cfbf944bf160f93f78bcdcfc537cd6da",
       },
       data: data,
     };
-    const results = [];
-    const res = await axios.request(config);
-    const $ = cheerio.load(res.data);
-    const title = $("h2").text();
-    if (!title) {
-      return {
-        success: false,
-        message: "Gagal mengambil data periksa url anda!",
-      };
-    }
-    const img = $("img").attr("src");
-    $("a").each((index, element) => {
-      const link = $(element).attr("href");
-      const text = $(element).text();
-      if (link.includes("https://dl.tiktokio.com")) {
-        results.push({ text, link });
-      }
-    });
+
+    const response = await axios.request(config);
+    const result = response.data;
+    const links = {
+      thumbnail: result.data.video_img,
+      mp4: result.data.mp4,
+      mp3: result.data.mp3,
+    };
     return {
       success: true,
       message: "Berhasil mengambil data",
-      judul: title,
-      thumbnail: img,
-      link: results,
+      nick: result.data.nick,
+      judul: result.data.video_info,
+      links: links,
     };
   } catch (error) {
     console.log(error);
   }
 };
+
 module.exports = getData;
